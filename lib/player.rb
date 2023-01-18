86456d023c9e86cf01d32a98564359ea09f6d720
Ariani Plukaard <ari.plukaard@gmail.com>
2023-01-18 17:31:31 +1100

    Added method to update position to the player class and made it initialise with attributes hash

--- lib/player.rb
+++ lib/player.rb
@@ -1,10 +1,17 @@
 class Player
-  def initialize(name)
-    @name = name
-    @money = 16
+  attr_reader :name, :money, :position
+
+  def initialize(attributes = {})
+    @name = attributes[:name]
+    @money = attributes[:money] || 16
+    @position = attributes[:position] || 0
   end
 
   def update_money(number)
     @money += number
+  end
+
+  def update_position(roll, board_length)
+    @position = (@position + roll) % board_length
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,7 @@
 require "player"
 
 describe Player do
-  let(:player) { Player.new("Test Name") }
+  let(:player) { Player.new(name: "Test Name") }
   describe "#update_money" do
     context "given integer 1" do
       it "returns the player's money with 1 added" do
@@ -11,6 +11,24 @@ describe Player do
     context "given integer -2" do
       it "returns the player's money with 2 subtracted" do
         expect(player.update_money(-2)).to eq(14)
+      end
+    end
+  end
+  describe "#update_position" do
+    context "given the roll 3 and board length 9 (starting from position 0)" do
+      it "returns the players position moved 3 spaces" do
+        expect(player.update_position(3, 9)).to eq(3)
+      end
+    end
+    context "given the roll 6 and board length 5 (starting from position 0)" do
+      it "returns the players position moved 6 spaces - board wraps around" do
+        expect(player.update_position(6, 5)).to eq(1)
+      end
+    end
+    context "given the roll 13 and board length 6 (starting from position 2)" do
+      let(:player) { Player.new(name:"Test Name", position: 2) }
+      it "returns the players position moved 13 spaces - board wraps around more than once" do
+        expect(player.update_position(13, 6)).to eq(3)
       end
     end
   end
